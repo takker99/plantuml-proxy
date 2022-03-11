5591274d79379a05008f7fc92383ba1ed1a943f3
takker99 <37929109+takker99@users.noreply.github.com>
2022-03-11 17:27:46 +0900

    :bug: s/http/https

--- src/toPlantUMLURL.ts
+++ src/toPlantUMLURL.ts
@@ -3,6 +3,6 @@ import { encode64 } from "./encoder.ts";
 import { textToBuffer } from "./converter.ts";
 
 export const toPlantUMLURL = (uml: string, imageType: "png" | "svg") =>
-  `http://www.plantuml.com/plantuml/${imageType}/${
+  `https://www.plantuml.com/plantuml/${imageType}/${
     encode64(deflate(textToBuffer(uml), 9))
   }`;
