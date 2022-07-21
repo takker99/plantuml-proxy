ea014deec5f60ab4b3dd74aa00a597ce8d33898e
GitHub <noreply@github.com>
2022-07-21 00:42:41 +0000

    :arrow_up: update deno dependencies

--- src/deps.ts
+++ src/deps.ts
@@ -1,2 +1,2 @@
-export { createHash } from "https://deno.land/std@0.148.0/hash/mod.ts";
+export { createHash } from "https://deno.land/std@0.149.0/hash/mod.ts";
 export { deflate } from "https://deno.land/x/denoflate@1.1/mod.ts";
